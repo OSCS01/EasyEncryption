5ef9ae9d4c9237efd9025fc067fdce7e6a25d696
Daryl <ithinkmynameisdaryl@gmail.com>
2017-06-15 17:14:49 +0800

    goods

---
+++ EasyEncryption/EasyEncryption/Home.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace EasyEncryption
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(94, 232);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(941, 548);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 919);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Home";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
---
+++ EasyEncryption/EasyEncryption/Home.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyEncryption
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+            listView1.Columns.Add("File name",-2);
+            listView1.Columns.Add("Size", -2);
+            listView1.View = View.Details;
+        }
+
+        private void addItems(System.IO.FileInfo fi)
+        {
+            string[] row = { fi.Name, "" + fi.Length};
+            ListViewItem lvi = new ListViewItem(row);
+            listView1.Items.Add(lvi);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var FD = new OpenFileDialog();
+            if (FD.ShowDialog() == DialogResult.OK)
+            {
+
+                System.IO.FileInfo file = new System.IO.FileInfo(FD.FileName);
+                addItems(file);
+                
+            }
+        }
+    }
+}
